9b7cd9ae30df4c35d40d9451afce0c1df386c9aa
ygq <493650029@qq.com>
2020-06-22 21:04:17 +0800

    [ygq] should_create_game()

---
+++ src/test/java/com/twschool/practice/domain/repository/GameRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.twschool.practice.domain.repository;
+
+import com.twschool.practice.domain.GuessNumberGame;
+import com.twschool.practice.repository.GameRepository;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GameRepositoryTest {
+
+    @Test
+    public void should_create_game(){
+        GameRepository gameRepository = new GameRepository();
+        GuessNumberGame guessNumberGame = gameRepository.create();
+        Assert.assertNotNull(guessNumberGame);
+    }
+
+    @Test
+    public void should_find_game(){
+        GameRepository gameRepository = new GameRepository();
+        GuessNumberGame expectedGuessNumberGame = gameRepository.create();
+
+        GuessNumberGame guessNumberGame = gameRepository.find();
+
+        Assert.assertNotNull(guessNumberGame);
+        Assert.assertEquals(expectedGuessNumberGame,guessNumberGame);
+    }
+}
